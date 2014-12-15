efaf896c1f84f3de04381ff27817a10ec778eee1
RObert Rigler <riglerr@uni.coventry.ac.uk>
2014-12-15 18:13:54 +0000

    manystuff

--- 207SE-Networks-&-Security/Portfolio/Assignment_7/Cache/Caching/main.c
+++ 207SE-Networks-&-Security/Portfolio/Assignment_7/Cache/Caching/main.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
 #include "cache_reader.h"
 
 //Simple file display to show how easy it is to use the cached reader functions
+
 int main(){
   char c;
   int refill_count=0;
   int byte_count=0;
   //Open a file
-  cr_file* f = cr_open("text",20);
+  cr_file* f = cr_open("text.txt",20);
 
   //While there are useful bytes coming from it
   while((c=cr_read_byte(f))!=EOF){
@@ -15,6 +15,7 @@ int main(){
     printf("%c",c);
 
   }
+
 
   //Then close the file
   printf("\nByte Count: %d",f->byte_tot); // Displaying the total number of bytes read.
@@ -24,3 +25,4 @@ int main(){
   //And finish
   return 0;
 }
+
