8135d54c4b2e39ad6b48cc2cf88617ff2a06f199
RObert Rigler <riglerr@uni.coventry.ac.uk>
2015-01-10 14:49:49 +0000

    Commented and converted to .txt

--- 210CT_Programming/7 - HeapSort/src/com/uni/Main.java
+++ 210CT_Programming/7 - HeapSort/src/com/uni/Main.java
@@ -5,55 +5,9 @@ import com.uni.InputString;
 public class Main {
 
     public static void main(String[] args) {
-        evalRPN();
+        RPN.evalRPN();
 
     }
 
-   public static void evalRPN() {
 
-       InputString Is = new InputString();
-       MyStack MS = new MyStack();
-       String[] Tokens;
-       String Operators = "+-/*";
-
-
-
-           if (Is.setIn_String(Is.getIn_String())) {
-               if (Is.checkIn_String()) {
-
-                   Tokens = Is.Split_String();
-
-                   for (String t : Tokens) {
-                       if (Operators.contains(t)) {
-                           int a = MS.Pop();
-                           int b = MS.Pop();
-
-                           switch (t.charAt(0)) {
-
-                               case '+':
-                                   MS.Push(MathOps.add(a, b));
-                                   break;
-                               case '-':
-                                   MS.Push(MathOps.sub(b, a));
-                                   break;
-                               case '*':
-                                   MS.Push(MathOps.mul(a, b));
-                                   break;
-                               case '/':
-                                   MS.Push(MathOps.div(b, a));
-                                   break;
-
-
-                           }
-
-                       } else MS.Push(Integer.parseInt(t));
-                   }
-
-               } else System.out.println("String not correct");
-
-           } else System.out.println("String not set");
-
-           System.out.println(MS.Pop());
-
-   }
 }
