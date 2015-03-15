99a479466a0f6b9f9c947588e4f051c9971ad55c
Robert Rigler <riglerr@uni.coventry.ac.uk>
2015-03-15 12:59:42 +0000

    15/03/15 Begin

---
+++ 207SE-Networks-Security/Portfolio2/Week19/HashFunction/HashFunction/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashFunction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String myName = "Robert";
+            double sum = 0;
+
+
+            for (int i = 0; i < myName.Length; i++) {
+
+               sum += (myName[i]) * Math.Pow(i+5,i+1);
+
+            }
+            Console.WriteLine("Hash Function of " + myName + ": " + sum);
+            Console.ReadLine();
+        }
+    }
+}
